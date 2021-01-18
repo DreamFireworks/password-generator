3768a97281e9de0b2562aaaf48db21d675f57096
Serhan Eraslan <serhaneraslan98@gmail.com>
2021-01-18 05:04:33 +0300

    slicing password chars

--- pass1.py
+++ pass1.py
@@ -8,9 +8,36 @@ print('''
 *PASSWORD GENERATOR v1*
 ***********************
 ''')
-chars = 'abcçdefgğhıijklmnopqrsştuüvwxyzABCÇDEFĞGHIİJKLMNOPQRSTUÜVWXYZ0123456789!@£$%^&*().,?/'
+lower = "abcçdefgğhıijklmnopqrsştuüvwxyz"
+upper = "ABCÇDEFĞGHIİJKLMNOPQRSTUÜVWXYZ"
+numbers = "0123456789"
+symbols = " !@£$%^&*().,?/' "
 length = input('Lenght? : ')
 length = int(length)
+pass_arr = [0,0,0,0]
+try:
+    lower_ask = input("Lower case? Y or N")
+    if lower_ask != "Y" or lower_ask != "N":
+        raise "input error"
+    if lower_ask == 'Y':
+        pass_arr[0] = 1
+    elif lower_ask == "N":
+        pass_arr[0] = 0
+except:
+    print("Error")
+
+try:
+    lower_ask = input("Upper case? Y or N")
+    if lower_ask != "Y" or lower_ask != "N":
+        raise "input error"
+    if lower_ask == 'Y':
+        pass_arr[1] = 1
+    elif lower_ask == "N":
+        pass_arr[1] = 0
+except:
+    print("Error")
+
+
 print('Password is : ')
 password = ''
 def psw(length):
@@ -24,7 +51,7 @@ current_time = now.strftime("%D-(%H:%M:%S)")
 f=open("pass.txt","a+")
 f.write("Time: " + str(current_time) + " : ")
 f.write(password)
-f.write(""" 
+f.write("""
 """)
 f.close()
 print("'pass.txt' has been added to the: " + str(path))
