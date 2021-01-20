752bca6033b0c190a5e8bc3282eb4eeaaf9d3876
Serhan Eraslan <serhaneraslan98@gmail.com>
2021-01-20 23:23:46 +0300

    GUI added. But needs to be tidy

--- pass_gui.py
+++ pass_gui.py
@@ -12,7 +12,7 @@ path = os.getcwd()
 lower="abcçdefgğhıijklmnopqrsştuüvwxyz"       
 upper = "ABCÇDEFĞGHIİJKLMNOPQRSTUÜVWXYZ"
 numbers = "0123456789"
-symbols = " !@£$%^&*().,?/' "
+symbols = "!@£$%^&*().,?/' "
 
 
 window = Tk()
@@ -25,23 +25,32 @@ res=[1,0,0,0]
 
 def var_states():
    global res
+   global choices
    print("{}{}{}{}".format(lower1.get(), upper1.get(), num1.get(), symb1.get()))
    res=[int(lower1.get()), int(upper1.get()), int(num1.get()), int(symb1.get())]
    if(res[0] == 1):
     choices = choices + lower
+    choices="".join(choices)
    if(res[1] == 1):
     choices = choices + upper
+    choices="".join(choices)
    if(res[2] == 1):
     choices = choices + numbers
+    choices="".join(choices)
    if(res[3] == 1):
     choices = choices + symbols
+    choices="".join(choices)
+   if(res == [0,0,0,0]):
+    choices = choices + lower
+    choices="".join(choices)
+
   
 def psw():
+  var_states()
   global choices
-  for i in range(0,8):
-    password=""
+  password=""
+  for i in range(8):
     password += random.choice(choices)
-    
   print(choices)
   print(password)
   now = datetime.now()
@@ -49,10 +58,15 @@ def psw():
   f=open("pass.txt","a+")
   f.write("Time: " + str(current_time) + " : ")
   f.write(password)
-  f.write("""
-  """)
+  f.write("\n")
   f.close()
   messagebox.showinfo("Done","'pass.txt' has been added to the: " + str(path))
+
+def resett():
+    global choices
+    global password
+    choices=""
+    password=""
 
 Label(window, text="Password Attributes").grid(row=1, sticky=W)
 lower1 = IntVar()
@@ -63,13 +77,10 @@ num1 = IntVar()
 Checkbutton(window, text="Numbers {0123456789}", variable=num1).grid(row=4, sticky=W)
 symb1 = IntVar()
 Checkbutton(window, text="Symbols {!@£$%^&*().,?/'}", variable=symb1).grid(row=5, sticky=W)
-Button(window, text='Show', command=var_states).grid(row=8, sticky=W, pady=4)
-Button(window, text='Quit', command=window.quit).grid(row=8,column=1, sticky=W, pady=4)
+Button(window, text='Reset', command=resett).grid(row=8,column=1, sticky=W, pady=4)
+Button(window, text='Quit', command=window.quit).grid(row=8,column=2, sticky=W, pady=4)
 Button(window, text='Generate', command=psw).grid(row=7, sticky=W, pady=4)
 label1 = Label(window,text="Password")
 label1.grid(row=6,sticky=W, pady=4)
-
-
-
 
 window.mainloop()
