83741fa9da034c2318b295e2ab6dd62de245b31e
Serhan Eraslan <serhaneraslan98@gmail.com>
2021-01-21 06:34:59 +0300

    Update pass_gui.py

--- pass_gui.py
+++ pass_gui.py
@@ -16,7 +16,7 @@ symbols = "!@£$%^&*().,?/' "
 
 
 window = Tk()
-#window.geometry("500x200")
+window.geometry("500x200")
 window.title("Password Generator By Serhan Eraslan")
 window.resizable(width=False,
                  height=False)
@@ -68,15 +68,13 @@ def resett():
     choices=""
     password=""
 
-Label(window, text="Password Attributes").grid(row=1, sticky=W)
-lower1 = IntVar()
-Checkbutton(window, text="Lower Case {abc...z}", variable=lower1).grid(row=2, sticky=W)
-upper1 = IntVar()
-Checkbutton(window, text="Upper Case {ABC...Z}", variable=upper1).grid(row=3, sticky=W)
+
+
+Checkbutton(window,  variable=upper1).grid(row=3, sticky=W)
 num1 = IntVar()
-Checkbutton(window, text="Numbers {0123456789}", variable=num1).grid(row=4, sticky=W)
+Checkbutton(window,  variable=num1).grid(row=4, sticky=W)
 symb1 = IntVar()
-Checkbutton(window, text="Symbols {!@£$%^&*().,?/'}", variable=symb1).grid(row=5, sticky=W)
+Checkbutton(window,  variable=symb1).grid(row=5, sticky=W)
 Button(window, text='Reset', command=resett).grid(row=8,column=1, sticky=W, pady=4)
 Button(window, text='Quit', command=window.quit).grid(row=8,column=2, sticky=W, pady=4)
 Button(window, text='Generate', command=psw).grid(row=7, sticky=W, pady=4)
@@ -84,3 +82,78 @@ label1 = Label(window,text="Password")
 label1.grid(row=6,sticky=W, pady=4)
 
 window.mainloop()
+
+
+IntroText='''
+***********************
+*PASSWORD GENERATOR v1*
+********Serhan Eraslan*
+'''
+
+Intro = Label(window,Text=IntroText)
+Intro.place(x=5,
+            y=0,
+            width=190,
+            height=50,
+            justify="center",
+            bg="#efefef")
+            
+            
+lower1 = IntVar()
+lower_box = Checkbutton(window, text="Lower Case {abc...z}", variable=lower1)
+lower_box.place(row=2, sticky=W)
+
+
+upper1 = IntVar()
+upper_box = Checkbutton(window, text="Upper Case {ABC...Z}", variable=lower1)
+upper_box.place(x=60,
+            y=0,
+            width=190,
+            height=20,
+            bg="#efefef")
+
+
+
+num1 = IntVar()
+num_box = Checkbutton(window, text="Numbers {0123456789}", variable=lower1)
+num_box.place(x=85,
+            y=0,
+            width=190,
+            height=20,
+            bg="#efefef")
+
+
+
+symb1 = IntVar()
+symbl_box = Checkbutton(window, text="Symbols {!@£$%^&*().,?/'}", variable=lower1)
+symbl_box.place(x=110,
+            y=0,
+            width=190,
+            height=20,
+            bg="#efefef")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
